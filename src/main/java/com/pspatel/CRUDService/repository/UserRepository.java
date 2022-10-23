b4c9a621af1ac3020c810c028087e0e2171341f0
pspatel602 <pspatel602@gmail.com>
2022-10-24 00:27:51 +0530

    added email verification support

--- src/main/java/com/pspatel/CRUDService/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pspatel.CRUDService.repository;
-
-import com.pspatel.CRUDService.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-public interface UserRepository extends MongoRepository<User, String> {}
